a1e5b83f2cabba320ef367c541e44a0dc06e3c59
karishma <karishma1997.s@gmail.com>
2023-03-23 20:38:12 +0530

    user info api and cors

--- app.js
+++ app.js
@@ -18,7 +18,13 @@ mongoose
 //Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Arena app listening on port ${process.env.PORT}`);
--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
-const { signup, signIn, adminSignup } = require('../controllers/auth-controller');
+const { signup, signIn, adminSignup, userInfo } = require('../controllers/auth-controller');
 const { validateRequest } = require('../middlewares/helper');
-
+//user register
 router.post(
   '/signup',
   [
@@ -11,6 +11,7 @@ router.post(
       min: 3,
     }),
     check('email', 'Enter valid email').isEmail(),
+    check('phone', 'Enter valid phone number').isLength({ min: 10 }),
     check('password', 'Password should be at least 6 characters.').isLength({
       min: 6,
     }),
@@ -19,7 +20,12 @@ router.post(
   signup
 );
 
-router.post('/signin', [check('email', 'Enter valid email').isEmail()], signIn);
+//user login
+
+router.post('/signin', [check('email', 'Enter valid email').isEmail(),
+check('password', 'Password should be at least 6 characters.').isLength({
+  min: 6,
+}),], signIn);
 
 //admin signup
 router.post('/admin/signup',
@@ -27,6 +33,7 @@ router.post('/admin/signup',
     check('name', 'Name should be at least 3 characters').isLength({
       min: 3,
     }),
+    check('phone', 'Enter valid phone number').isLength({ min: 10, max: 10 }),
     check('email', 'Enter valid email').isEmail(),
     check('password', 'Password should be at least 6 characters.').isLength({
       min: 6,
@@ -34,4 +41,23 @@ router.post('/admin/signup',
   ],
   validateRequest,
   adminSignup)
+
+//user information
+
+router.post('/userInfo',
+  [
+    check('address', 'Address should be at least 3 characters').isLength({
+      min: 10
+    }),
+    check('city', 'City should be at least 3 characters').isLength({
+      min: 3
+    }),
+    check('phone', 'Enter valid phone number').isLength({ min: 10, max: 10 }),
+    check('email', 'Enter valid email').isEmail(),
+    check('pincode', 'Pincode should be at least 5 characters.').isLength({
+      min: 5,
+    }),
+  ],
+  validateRequest,
+  userInfo)
 module.exports = router;
